4452d1e4ab9b20000372f078c6b67cef675162d3
Chester Gillon <chester.gillon@metronet.co.uk>
2014-11-23 17:30:57 +0000

    Change cmat_mulx_fixed_dimension_matrix_multiplies and cmat_mulx_fixed_dimension_accumulate_matrix_multiplies to be passed arrays of pointers to each row of the matrices, so may be used with rows which are are not equally spaced in memory

--- c_avx_fixed_dimension_matrix_multiply.c
+++ c_avx_fixed_dimension_matrix_multiply.c
@@ -10,10 +10,9 @@
 typedef struct
 {
     SAL_i32 nr_c, nc_c, dot_product_length;
-    SAL_cf32 *left_matrix;
-    SAL_cf32 *right_matrix;
-    SAL_cf32 *output_matrix;
-    SAL_i32 left_matrix_tcols, right_matrix_tcols, output_matrix_tcols;
+    SAL_cf32 *left_matrix_rows[CMAT_MULX_FIXED_DIMENSION_MAX_NR_C];
+    SAL_cf32 *right_matrix_rows[CMAT_MULX_FIXED_DIMENSION_MAX_DOT_PRODUCT_LENGTH];
+    SAL_cf32 *output_matrix_rows[CMAT_MULX_FIXED_DIMENSION_MAX_NR_C];
     cmat_mulx_fixed_dimension_func cmat_mulx_func;
 } matrix_context;
 
@@ -21,9 +20,9 @@ static void timed_c_matrix_multiply (void *arg)
 {
     matrix_context *const context = (matrix_context *) arg;
 
-    (*context->cmat_mulx_func) (context->left_matrix, context->left_matrix_tcols,
-                                context->right_matrix, context->right_matrix_tcols,
-                                context->output_matrix, context->output_matrix_tcols,
+    (*context->cmat_mulx_func) (context->left_matrix_rows,
+                                context->right_matrix_rows,
+                                context->output_matrix_rows,
                                 context->nc_c);
 }
 
@@ -35,9 +34,12 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
     const mxArray *const block_other_cpus_in = prhs[3];
     const mwSize *left_matrix_dimensions;
     const mwSize *right_matrix_dimensions;
+    SAL_i32 left_matrix_tcols, right_matrix_tcols, output_matrix_tcols;
+    SAL_cf32 *left_matrix, *right_matrix, *output_matrix;
     mxArray *mx_output_matrix;
     mxArray *timing_results;
     matrix_context context;
+    SAL_i32 row_index;
 
     if (nlhs != 2)
     {
@@ -75,20 +77,30 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
     
     if (context.cmat_mulx_func != NULL)
     {
-        context.left_matrix = copy_mx_to_cf32_matrix (left_matrix_in, &context.left_matrix_tcols);
-        context.right_matrix = copy_mx_to_cf32_matrix (right_matrix_in, &context.right_matrix_tcols);
-        context.output_matrix_tcols = (context.nc_c + 3) & ~3;
-        context.output_matrix = mxCalloc_and_touch (context.nr_c * context.output_matrix_tcols, sizeof(SAL_cf32));
+        left_matrix = copy_mx_to_cf32_matrix (left_matrix_in, &left_matrix_tcols);
+        right_matrix = copy_mx_to_cf32_matrix (right_matrix_in, &right_matrix_tcols);
+        output_matrix_tcols = (context.nc_c + 3) & ~3;
+        output_matrix = mxCalloc_and_touch (context.nr_c * output_matrix_tcols, sizeof(SAL_cf32));
+        
+        for (row_index = 0; row_index < context.nr_c; row_index++)
+        {
+            context.left_matrix_rows[row_index] = left_matrix + (row_index * left_matrix_tcols);
+            context.output_matrix_rows[row_index] = output_matrix + (row_index * output_matrix_tcols);
+        }
+        for (row_index = 0; row_index < context.dot_product_length; row_index++)
+        {
+            context.right_matrix_rows[row_index] = right_matrix + (row_index * right_matrix_tcols);
+        }
 
         timing_results = time_matrix_multiply (timed_c_matrix_multiply, &context, mxGetScalar (num_timed_iterations_in),
                                                mxGetScalar (block_other_cpus_in));
 
         mx_output_matrix = mxCreateNumericMatrix (context.nr_c, context.nc_c, mxSINGLE_CLASS, mxCOMPLEX);
-        copy_cf32_to_mx_matrix (context.output_matrix, context.output_matrix_tcols, mx_output_matrix);
+        copy_cf32_to_mx_matrix (output_matrix, output_matrix_tcols, mx_output_matrix);
     
-        mxFree (context.left_matrix);
-        mxFree (context.right_matrix);
-        mxFree (context.output_matrix);
+        mxFree (left_matrix);
+        mxFree (right_matrix);
+        mxFree (output_matrix);
     }
     else
     {
